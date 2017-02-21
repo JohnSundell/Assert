35264be9b358025f9fd2d1ef1f71a8693f7bfb59
John Sundell <john@sundell.co>
2017-02-21 17:28:31 +0100

    Make functions public

--- Sources/Assert.swift
+++ Sources/Assert.swift
@@ -17,10 +17,10 @@ import XCTest
  *  - parameter expression: The expression that should throw an error
  *  - parameter errorExpression: An expression resulting in an error that is expected to be thrown
  */
-func assert<T, E: Error>(at file: StaticString = #file,
-            line: UInt = #line,
-            _ expression: @autoclosure () throws -> T,
-            throwsError errorExpression: @autoclosure () -> E) where E: Equatable {
+public func assert<T, E: Error>(at file: StaticString = #file,
+                                line: UInt = #line,
+                                _ expression: @autoclosure () throws -> T,
+                                throwsError errorExpression: @autoclosure () -> E) where E: Equatable {
     do {
         _ = try expression()
         XCTFail("Expected expression to throw", file: file, line: line)
@@ -46,10 +46,10 @@ func assert<T, E: Error>(at file: StaticString = #file,
  *  - parameter errorExpression: An expression resulting in an error that is expected to be thrown
  *  - closure: The closure that should thrown an error
  */
-func assertErrorThrown<T, E: Error>(at file: StaticString = #file,
-                       line: UInt = #line,
-                       _ errorExpression: @autoclosure () -> E,
-                       by closure: () throws -> T) where E: Equatable {
+public func assertErrorThrown<T, E: Error>(at file: StaticString = #file,
+                              line: UInt = #line,
+                              _ errorExpression: @autoclosure () -> E,
+                              by closure: () throws -> T) where E: Equatable {
     assert(at: file, line: line, try closure(), throwsError: errorExpression)
 }
 
@@ -62,7 +62,7 @@ func assertErrorThrown<T, E: Error>(at file: StaticString = #file,
  *  - parameter line: The line number at which the assert should take place (automatically inferred)
  *  - parameter closure: The closure that shouldn't throw an error
  */
-func assertNoErrorThrown<T>(at file: StaticString = #file, line: UInt = #line, from closure: () throws -> T) {
+public func assertNoErrorThrown<T>(at file: StaticString = #file, line: UInt = #line, from closure: () throws -> T) {
     do {
         _ = try closure()
     } catch {
